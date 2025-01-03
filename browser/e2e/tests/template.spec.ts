7813e4a0dab74c43a32ac8c6ecbe49ca8099bcd2
Joep Meindertsma <joep@ontola.io>
2025-01-03 18:14:31 +0100

    Add SvelteKit to templte e2e test

--- browser/e2e/tests/template.spec.ts
+++ browser/e2e/tests/template.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
-import { execSync } from 'child_process';
+import { exec } from 'child_process';
 import {
   before,
   makeDrivePublic,
@@ -13,23 +13,68 @@ import fs from 'node:fs';
 import { spawn, type ChildProcess } from 'node:child_process';
 import path from 'node:path';
 import kill from 'kill-port';
+import { promisify } from 'util';
+import { log } from 'node:console';
 
+const execAsync = promisify(exec);
 const TEMPLATE_DIR_NAME = 'template-tests';
+// test.describe.configure({ mode: 'serial' });
 
-test.describe.configure({ mode: 'serial' });
+async function setupTemplateSite(
+  templateDir: string,
+  serverUrl: string,
+  siteType: string,
+) {
+  if (!fs.existsSync(templateDir)) {
+    fs.mkdirSync(templateDir);
+  }
 
-const waitForNextServer = (
+  await execAsync('pnpm link ../create-template');
+  await execAsync(
+    `pnpm exec create-template ${templateDir}/${siteType} --template ${siteType} --server-url ${serverUrl}`,
+  );
+
+  const sitePath = `${templateDir}/${siteType}`;
+  await execAsync('pnpm install', { cwd: sitePath });
+  await execAsync('pnpm link ../../../cli', { cwd: sitePath });
+  await execAsync('pnpm link ../../../lib', { cwd: sitePath });
+
+  if (siteType === 'nextjs-site') {
+    await execAsync('pnpm link ../../../react', { cwd: sitePath });
+  } else if (siteType === 'sveltekit-site') {
+    await execAsync('pnpm link ../../../svelte', { cwd: sitePath });
+  }
+
+  await execAsync('pnpm update-ontologies', { cwd: sitePath });
+}
+
+function startServer(templateDir: string, siteType: string) {
+  // Adjust runtime commands per template
+  const command =
+    siteType === 'nextjs-site'
+      ? 'pnpm run build && pnpm start'
+      : 'pnpm run build && NO_COLOR=1 pnpm preview';
+
+  return spawn(command, {
+    cwd: `${templateDir}/${siteType}`,
+    shell: true,
+  });
+}
+
+const waitForServer = (
   childProcess: ChildProcess,
   timeout = 30000,
 ): Promise<string> => {
   return new Promise((resolve, reject) => {
     const timeoutId = setTimeout(() => {
       childProcess.kill(); // Kill the process if it times out
-      reject(new Error('Next.js server took too long to start.'));
+      reject(new Error('Server took too long to start.'));
     }, timeout);
 
     childProcess.stdout?.on('data', data => {
       const message = data.toString();
+
+      log(message);
 
       const match = message.match(/http:\/\/localhost:\d+/);
 
@@ -45,9 +90,7 @@ const waitForNextServer = (
 
       if (errorMessage.includes('error')) {
         clearTimeout(timeoutId); // Clear the timeout when rejecting
-        reject(
-          new Error(`Next.js server encountered an error: ${errorMessage}`),
-        );
+        reject(new Error(`Server encountered an error: ${errorMessage}`));
       }
     });
 
@@ -55,7 +98,7 @@ const waitForNextServer = (
       clearTimeout(timeoutId); // Clear the timeout when the process exits
 
       if (code !== 0) {
-        reject(new Error(`Next.js server process exited with code ${code}`));
+        reject(new Error(`Server process exited with code ${code}`));
       }
     });
   });
@@ -64,7 +107,7 @@ const waitForNextServer = (
 test.describe('Create Next.js Template', () => {
   test.beforeEach(before);
 
-  test('apply template', async ({ page }) => {
+  test('apply next-js template', async ({ page }) => {
     test.slow();
     await signIn(page);
     const drive = await newDrive(page);
@@ -82,40 +125,13 @@ test.describe('Create Next.js Template', () => {
     });
     await applyTemplateButton.click();
 
-    if (!fs.existsSync(TEMPLATE_DIR_NAME)) {
-      fs.mkdirSync(TEMPLATE_DIR_NAME);
-    }
+    await setupTemplateSite(TEMPLATE_DIR_NAME, drive.driveURL, 'nextjs-site');
 
-    execSync('pnpm link ../create-template');
-    execSync(
-      `pnpm exec create-template ${TEMPLATE_DIR_NAME}/nextjs-template --template nextjs-site --server-url ${drive.driveURL}`,
-    );
-    execSync(`pnpm install`, {
-      cwd: `${TEMPLATE_DIR_NAME}/nextjs-template`,
-    });
-    execSync('pnpm link ../../../cli', {
-      cwd: `${TEMPLATE_DIR_NAME}/nextjs-template`,
-    });
-    execSync('pnpm link ../../../lib', {
-      cwd: `${TEMPLATE_DIR_NAME}/nextjs-template`,
-    });
-    execSync('pnpm link ../../../react', {
-      cwd: `${TEMPLATE_DIR_NAME}/nextjs-template`,
-    });
-
-    execSync(`pnpm update-ontologies`, {
-      cwd: `${TEMPLATE_DIR_NAME}/nextjs-template`,
-    });
-
-    const child = spawn('pnpm run build && pnpm start', {
-      cwd: `${TEMPLATE_DIR_NAME}/nextjs-template`,
-
-      shell: true,
-    });
+    const child = startServer(TEMPLATE_DIR_NAME, 'nextjs-site');
 
     try {
       //start server
-      const url = await waitForNextServer(child);
+      const url = await waitForServer(child);
 
       // check if the server is running
       const response = await page.goto(url);
@@ -137,9 +153,8 @@ test.describe('Create Next.js Template', () => {
       expect(blogScanResults.violations).toEqual([]);
 
       // Search for a blogpost
-      const searchInput = page.locator(
-        'input[aria-label="Search blogposts..."]',
-      );
+      const searchInput = page.getByRole('searchbox');
+
       await searchInput.fill('balloon');
       await expect(page.locator('body')).toContainText('Balloon');
       await expect(page.locator('body')).not.toContainText('coffee');
@@ -149,7 +164,12 @@ test.describe('Create Next.js Template', () => {
   });
 
   test.afterEach(async () => {
-    const dirPath = path.join(__dirname, '..', TEMPLATE_DIR_NAME);
+    const dirPath = path.join(
+      __dirname,
+      '..',
+      TEMPLATE_DIR_NAME,
+      'nextjs-site',
+    );
 
     try {
       await fs.promises.rm(dirPath, { recursive: true, force: true });
@@ -158,5 +178,85 @@ test.describe('Create Next.js Template', () => {
     }
 
     await kill(3000);
+  });
+});
+
+test.describe('Create SvelteKit Template', () => {
+  test.beforeEach(before);
+
+  test('apply sveltekit template', async ({ page }) => {
+    test.slow();
+    await signIn(page);
+    const drive = await newDrive(page);
+    await makeDrivePublic(page);
+
+    // Apply the template in data browser
+    await page.getByTestId(sideBarNewResourceTestId).click();
+    await expect(page).toHaveURL(`${FRONTEND_URL}/app/new`);
+
+    const button = page.getByTestId('template-button');
+    await button.click();
+
+    const applyTemplateButton = page.getByRole('button', {
+      name: 'Apply template',
+    });
+    await applyTemplateButton.click();
+
+    await setupTemplateSite(
+      TEMPLATE_DIR_NAME,
+      drive.driveURL,
+      'sveltekit-site',
+    );
+
+    const child = startServer(TEMPLATE_DIR_NAME, 'sveltekit-site');
+
+    try {
+      //start server
+      const url = await waitForServer(child);
+
+      // check if the server is running
+      const response = await page.goto(url);
+      expect(response?.status()).toBe(200);
+
+      // Check if home is following wcag AA standards
+      const homeScanResults = await new AxeBuilder({ page }).analyze();
+
+      expect(homeScanResults.violations).toEqual([]);
+
+      await expect(page.locator('body')).toContainText(
+        'This is a template site generated with @tomic/template.',
+      );
+
+      await page.goto(`${url}/blog`);
+
+      // Check if blog is following wcag AA standards
+      const blogScanResults = await new AxeBuilder({ page }).analyze();
+      expect(blogScanResults.violations).toEqual([]);
+
+      // Search for a blogpost
+      const searchInput = page.getByRole('searchbox');
+      await searchInput.fill('balloon');
+      await expect(page.locator('body')).toContainText('Balloon');
+      await expect(page.locator('body')).not.toContainText('coffee');
+    } finally {
+      child.kill();
+    }
+  });
+
+  test.afterEach(async () => {
+    const dirPath = path.join(
+      __dirname,
+      '..',
+      TEMPLATE_DIR_NAME,
+      'sveltekit-site',
+    );
+
+    try {
+      await fs.promises.rm(dirPath, { recursive: true, force: true });
+    } catch (error) {
+      console.error(`Failed to delete ${TEMPLATE_DIR_NAME}:`, error);
+    }
+
+    await kill(4173);
   });
 });
