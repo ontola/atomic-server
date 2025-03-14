797684a23255098ad1b3b84c8eda7d9c8cf41b79
Joep Meindertsma <joep@ontola.io>
2025-03-14 15:02:10 +0100

    Fix subscribe warning

--- server/src/commit_monitor.rs
+++ server/src/commit_monitor.rs
@@ -61,8 +61,11 @@ impl Handler<Subscribe> for CommitMonitor {
                 let self_url = store
                     .get_base_domain()
                     .expect("No base url set in Commit Monitor");
-                if !msg.subject.as_str().starts_with(&self_url) {
-                    tracing::warn!("can't subscribe to external resource");
+                if !msg.subject.as_str().starts_with(&self_url)
+                    && !msg.subject.as_str().starts_with("did:")
+                    && !msg.subject.as_str().starts_with("internal:")
+                {
+                    tracing::warn!("can't subscribe to external resource: {}", msg.subject);
                     return None;
                 }
                 match store.get_resource(&msg.subject).await {
