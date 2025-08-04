f4331aa535c92e4d0e642f42d453338f2913fc7d
Polle Pas <polleps@gmail.com>
2025-08-04 16:54:41 +0200

    fix parse test

--- browser/lib/src/parse.test.ts
+++ browser/lib/src/parse.test.ts
@@ -4,7 +4,6 @@ import { JSONADParser } from './parse.js';
 const EXAMPLE_SUBJECT = 'http://example.com/1';
 const EXAMPLE_SUBJECT2 = 'http://example.com/2';
 const EXAMPLE_SUBJECT3 = 'http://example.com/3';
-const EXAMPLE_SUBJECT4 = 'http://example.com/4';
 
 const STRING_PROPERTY = 'http://example.com/some-string-property';
 const NUMBER_PROPERTY = 'http://example.com/some-number-property';
@@ -21,69 +20,11 @@ describe('parse.ts', () => {
     };
 
     const parser = new JSONADParser();
-    const [resource] = parser.parseObject(jsonObject);
+    const [resource] = parser.parse(jsonObject);
 
     expect(resource.get(STRING_PROPERTY)).toBe('Hoi');
     expect(resource.get(NUMBER_PROPERTY)).toBe(10);
     expect(resource.get(BOOLEAN_PROPERTY)).toBe(true);
-  });
-
-  it('parses a JSON-AD object with a nested resource', ({ expect }) => {
-    const jsonObjectWithID = {
-      '@id': EXAMPLE_SUBJECT,
-      [NESTED_RESOURCE_PROPERTY]: {
-        '@id': EXAMPLE_SUBJECT2,
-        [STRING_PROPERTY]: 'Hoi',
-      },
-    };
-
-    const jsonObjectWithoutID = {
-      '@id': EXAMPLE_SUBJECT,
-      [NESTED_RESOURCE_PROPERTY]: {
-        [STRING_PROPERTY]: 'Hoi',
-      },
-    };
-
-    const jsonWithArrayOfResources = {
-      '@id': EXAMPLE_SUBJECT,
-      [NESTED_RESOURCE_PROPERTY]: [
-        {
-          '@id': EXAMPLE_SUBJECT2,
-          [STRING_PROPERTY]: 'Hoi',
-        },
-        EXAMPLE_SUBJECT3,
-        {
-          [STRING_PROPERTY]: 'Hoi',
-        },
-      ],
-    };
-
-    const parser = new JSONADParser();
-    const [resource1, parsedResources1] = parser.parseObject(jsonObjectWithID);
-    expect(resource1.get(NESTED_RESOURCE_PROPERTY)).toBe(EXAMPLE_SUBJECT2);
-    expect(parsedResources1).toHaveLength(2);
-    expect(parsedResources1[1].get(STRING_PROPERTY)).toBe('Hoi');
-
-    const [resource2, parsedResources2] =
-      parser.parseObject(jsonObjectWithoutID);
-
-    expect(resource2.get(NESTED_RESOURCE_PROPERTY)).toEqual({
-      [STRING_PROPERTY]: 'Hoi',
-    });
-
-    expect(parsedResources2).toHaveLength(1);
-
-    const [resource3, parsedResources3] = parser.parseObject(
-      jsonWithArrayOfResources,
-    );
-
-    expect(resource3.get(NESTED_RESOURCE_PROPERTY)).toEqual([
-      EXAMPLE_SUBJECT2,
-      EXAMPLE_SUBJECT3,
-      { [STRING_PROPERTY]: 'Hoi' },
-    ]);
-
-    expect(parsedResources3).toHaveLength(2);
   });
 
   it('parses an array of jsonObjects', ({ expect }) => {
@@ -100,17 +41,15 @@ describe('parse.ts', () => {
         '@id': EXAMPLE_SUBJECT3,
         [STRING_PROPERTY]: 'Third Resource',
         [NESTED_RESOURCE_PROPERTY]: {
-          '@id': EXAMPLE_SUBJECT4,
-          [STRING_PROPERTY]: 'Fourth Resource',
+          [STRING_PROPERTY]: 'Nested Resource',
         },
       },
     ];
 
     const parser = new JSONADParser();
-    const [resources, parsedResources] = parser.parseArray(array);
+    const resources = parser.parse(array);
 
     expect(resources).toHaveLength(3);
-    expect(parsedResources).toHaveLength(4);
   });
 
   it('Handles resources without an ID', ({ expect }) => {
@@ -119,7 +58,7 @@ describe('parse.ts', () => {
     };
 
     const parser = new JSONADParser();
-    const [resource] = parser.parseObject(jsonObject, 'my-new-id');
+    const [resource] = parser.parse(jsonObject, 'my-new-id');
 
     expect(resource.get(STRING_PROPERTY)).toBe('Hoi');
     expect(resource.subject).toBe('my-new-id');
